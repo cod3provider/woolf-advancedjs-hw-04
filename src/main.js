ba1ab11436be0989d29cef458836ae2106379d3d
Ilya Bondarchuk <cod3provider@gmail.com>
2023-12-02 12:56:30 +0200

    add listener to form

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-import { API_KEY, BASE_URL } from './api/keys.js';
+import { fetchImages } from './api/fetch-api.js';
 
 const form = document.querySelector('.search-form');
 
+function onHandleSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.currentTarget);
+  const data = {};
+
+  formData.forEach((value, name) => {
+    data[name] = value;
+  });
+
+  fetchImages(data)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+}
+
+form.addEventListener('submit', onHandleSubmit);
