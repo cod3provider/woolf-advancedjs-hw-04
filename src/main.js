039d3df4c52aa9472d1b0428275e5e4a17f5baa5
Ilya Bondarchuk <cod3provider@gmail.com>
2023-12-02 17:17:17 +0200

    add gallery render

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { fetchImages } from './api/fetch-api.js';
 
 const form = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+
 
 function onHandleSubmit(e) {
   e.preventDefault();
+  // gallery.innerHTML = '';
 
   const formData = new FormData(e.currentTarget);
   const data = {};
@@ -13,8 +16,42 @@ function onHandleSubmit(e) {
   });
 
   fetchImages(data)
-  .then(data => console.log(data))
+  .then(data => {
+    console.log(data);
+    const markup = createMarkup(data.hits);
+    console.log(markup);
+    gallery.insertAdjacentHTML('afterbegin', markup);
+  })
   .catch(err => console.log(err));
+}
+
+function createMarkup(arr) {
+  return arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
+      return `
+    <div class='photo-card'>
+      <img src='${webformatURL}' alt='${tags}' loading='lazy' />
+      <div class='info'>
+        <p class='info-item'>
+          <b>Likes</b>
+          ${likes}
+        </p>
+        <p class='info-item'>
+          <b>Views</b>
+          ${views}
+        </p>
+        <p class='info-item'>
+          <b>Comments</b>
+          ${comments}
+        </p>
+        <p class='info-item'>
+          <b>Downloads</b>
+          ${downloads}
+        </p>
+      </div>
+    </div>
+  `;
+    }
+  )
 }
 
 form.addEventListener('submit', onHandleSubmit);
