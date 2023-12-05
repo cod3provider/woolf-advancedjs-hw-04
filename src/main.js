c31895e7e9720187819101946798fb725185fc3d
Ilya Bondarchuk <cod3provider@gmail.com>
2023-12-05 10:24:37 +0200

    add izitoast to error feature

--- src/main.js
+++ src/main.js
@@ -1,30 +1,45 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 import { fetchImages } from './api/fetch-api.js';
 import { createMarkup } from './api/createMarkup.js';
 
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 
+function errorToast () {
+  iziToast.error({
+    position: 'topRight',
+    title: 'Error',
+    message: 'Sorry, there are no images matching your search query. Please try again.'
+  });
+}
 
 function onHandleSubmit(e) {
   e.preventDefault();
-  // gallery.innerHTML = '';
+  gallery.innerHTML = '';
+  const query = e.currentTarget.elements.searchQuery.value.trim();
 
-  const formData = new FormData(e.currentTarget);
-  const data = {};
+    fetchImages(query)
+    .then(data => {
+      console.log(data);
 
-  formData.forEach((value, name) => {
-    data[name] = value;
-  });
+      if (!data.hits.length) {
+        errorToast()
+      }
 
-  fetchImages(data)
-  .then(data => {
-    console.log(data);
-    const markup = createMarkup(data.hits);
-    console.log(markup);
-    gallery.insertAdjacentHTML('afterbegin', markup);
-  })
-  .catch(err => console.log(err));
+      const markup = createMarkup(data.hits);
+      // console.log(markup);
+
+      gallery.insertAdjacentHTML('afterbegin', markup);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  // } catch(err) {
+  //   console.log(err);
+  // }
 }
+
 
 
 
