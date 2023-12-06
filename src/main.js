405b7b8907e9891d909d1492dfae73290ae00d0f
Ilya Bondarchuk <cod3provider@gmail.com>
2023-12-06 20:10:05 +0200

    small fixes: fix numbers of requests' pages when add new request, page not canceled to first, change promise to try...catch

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchImages } from './api/fetch-api.js';
 import { createMarkup } from './api/createMarkup.js';
 import { endOfResultsInfo, toastEnterSearch, toastError, toastFoundedImages, toastInfoSearch } from './api/toasts.js';
@@ -19,10 +19,12 @@ let isLoading = false;
 form.addEventListener('submit', onHandleSubmit);
 loadMoreBtn.addEventListener('click', loadMore);
 
-function onHandleSubmit(e) {
+async function onHandleSubmit(e) {
   e.preventDefault();
   loadMoreBtn.classList.add('hidden');
   const query = e.currentTarget.elements.searchQuery.value.trim();
+  numberPage = 1;
+  let data;
 
   if (query === '') {
     toastEnterSearch();
@@ -36,31 +38,32 @@ function onHandleSubmit(e) {
   gallery.innerHTML = '';
   existSearchQuery = query;
 
-  fetchImages(query, numberPage)
-  .then(data => {
-    if (!data.totalHits) {
-      toastError();
-      return;
-    }
-
-    if (numberPage === 1) {
-      toastFoundedImages(data);
-    }
-
-    if (data.totalHits <= numberPage * PER_PAGE) {
-      endOfResultsInfo();
-    }
-
-    if (data.totalHits > numberPage * PER_PAGE) {
-      loadMoreBtn.classList.remove('hidden');
-    }
-    const markup = createMarkup(data.hits);
-    gallery.insertAdjacentHTML('afterbegin', markup);
-    lightbox = new SimpleLightbox(`.gallery a`);
-  })
-  .catch(err => {
+  try {
+    data = await fetchImages(query, numberPage);
+  }
+  catch (err) {
     console.log(err);
-  });
+  }
+
+  if (!data.totalHits) {
+    toastError();
+    return;
+  }
+
+  if (numberPage === 1) {
+    toastFoundedImages(data);
+  }
+
+  if (data.totalHits <= numberPage * PER_PAGE) {
+    endOfResultsInfo();
+  }
+
+  if (data.totalHits > numberPage * PER_PAGE) {
+    loadMoreBtn.classList.remove('hidden');
+  }
+  const markup = createMarkup(data.hits);
+  gallery.insertAdjacentHTML('afterbegin', markup);
+  lightbox = new SimpleLightbox(`.gallery a`);
 }
 
 export async function loadMore() {
@@ -69,31 +72,20 @@ export async function loadMore() {
   }
 
   isLoading = true;
-  // numberPage += 1;
-
   const searchQuery = form.elements.searchQuery.value.trim();
   const data = await fetchImages(searchQuery, numberPage += 1);
-
   gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
   lightbox.refresh();
   loadMoreBtn.classList.remove('hidden');
 
   const { height: cardHeight } = document
-  .querySelector(".gallery")
+  .querySelector('.gallery')
   .firstElementChild.getBoundingClientRect();
 
   window.scrollBy({
     top: cardHeight * 2,
-    behavior: "smooth",
+    behavior: 'smooth',
   });
 
   isLoading = false;
 }
-
-// window.addEventListener('scroll', () => {
-//   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-//
-//   if (clientHeight + scrollTop >= scrollHeight * 0.9) {
-//     loadMore();
-//   }
-// });
